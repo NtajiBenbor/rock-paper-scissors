1cb97bdf956013429067dd0ca0fb99c472adc420
Benedict Ntaji <108682913+NtajiBenbor@users.noreply.github.com>
2025-06-27 04:57:24 +0100

    Implement the playRound function
    this function is responsible for implementing a
    single round of the game.

---
+++ app.js
@@ -0,0 +1,52 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber )
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else if (randomNumber === 3) {
+    return "scissors";
+  }
+}
+
+// console.log(getComputerChoice())
+
+function getHumanChoice() {
+  let userInput = prompt('enter your choice:');
+  return userInput;
+}
+
+function playRound(playerChoice, computerChoice) {
+    let userChoice = playerChoice.toLowerCase();
+    if (userChoice === "scissors" && computerChoice === "paper") {
+      console.log(`you win! ${playerChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else if (userChoice === "paper" && computerChoice === "scissors") {
+      console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
+      computerScore++;
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+      console.log(`you win! ${playerChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else if (userChoice === "rock" && computerChoice === "paper") {
+      console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
+      computerScore++;
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+      console.log(`you win! ${playerChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+      console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
+      computerScore++;
+    } else if (playerChoice === computerChoice) {
+      console.log(`it is a tie`);
+    }
+  console.log(`playerchoice:${playerChoice}, computerchoice:${computerChoice}`);
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
