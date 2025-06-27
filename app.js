3882fa652a3e75d67a5e28cf0cefb5359b239507
Benedict Ntaji <108682913+NtajiBenbor@users.noreply.github.com>
2025-06-27 05:42:22 +0100

    remove console.log calls used for debugging

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber )
   if (randomNumber === 1) {
     return "rock";
   } else if (randomNumber === 2) {
@@ -12,43 +10,13 @@ function getComputerChoice() {
   }
 }
 
-// console.log(getComputerChoice())
+
 
 function getHumanChoice() {
   let userInput = prompt('enter your choice:');
   return userInput;
 }
 
-// function playRound(playerChoice, computerChoice) {
-//     let userChoice = playerChoice.toLowerCase();
-//     if (userChoice === "scissors" && computerChoice === "paper") {
-//       console.log(`you win! ${playerChoice} beats ${computerChoice}`);
-//       humanScore++;
-//     } else if (userChoice === "paper" && computerChoice === "scissors") {
-//       console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
-//       computerScore++;
-//     } else if (userChoice === "paper" && computerChoice === "rock") {
-//       console.log(`you win! ${playerChoice} beats ${computerChoice}`);
-//       humanScore++;
-//     } else if (userChoice === "rock" && computerChoice === "paper") {
-//       console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
-//       computerScore++;
-//     } else if (userChoice === "rock" && computerChoice === "scissors") {
-//       console.log(`you win! ${playerChoice} beats ${computerChoice}`);
-//       humanScore++;
-//     } else if (userChoice === "scissors" && computerChoice === "rock") {
-//       console.log(`you lost! ${computerChoice} beats ${playerChoice}`);
-//       computerScore++;
-//     } else if (playerChoice === computerChoice) {
-//       console.log(`it is a tie`);
-//     }
-//   console.log(`playerchoice:${playerChoice}, computerchoice:${computerChoice}`);
-// }
-
-// let humanSelection = getHumanChoice();
-// let computerSelection = getComputerChoice();
-
-// playRound(humanSelection,computerSelection);
 
 function playGame() {
   let humanScore = 0;
@@ -59,7 +27,6 @@ function playGame() {
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
-    console.log(`compscore:${computerScore}, humScore:${humanScore}`)
     counter++;
   }
   let winner =
